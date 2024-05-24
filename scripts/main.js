43771c5974ae04483eb5061a6610561b832e07bf
janhulka <st024713@student,cichnovabrno.cz>
2024-05-24 14:50:01 +0200

    animace, zkrasleni uvodniho slidu

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,7 @@ const quizLength = 3
 var quizAnswered = 0
 var quizRight = 0
 
-$('#congratulation-container').hide();
-$('#congratulation-ilustration').hide();
-$('#congratulation-textblock').hide();
-
-$('#cta-container').hide();
-$('#cta-ilustration').hide();
-$('#cta-textblock').hide();
-
-var swiper = new Swiper(".quizSwiper", {
+var quizSwiper = new Swiper(".quiz-swiper", {
     slidesPerView: quizLength,
     spaceBetween: 16,
     allowTouchMove: false,
@@ -20,34 +12,48 @@ var swiper = new Swiper(".quizSwiper", {
     },
 });
 
-swiper.on('slideChange', function () {
-    var swiperWrapper = document.getElementById("quiz-swiper-wrapper");
-    var activeSlide = swiperWrapper.getElementsByClassName("swiper-slide-next");
+$(document).ready(function(){
+    $("#preview-divider").draggable({axis: "x", containment: [385, 0, 1503, 0], drag: function(event, ui) {
+        var divider = $("#preview-divider").css("left");
+        var pos = Number(divider.replace("px", ""));
+        $("#preview-box-new").css({"left":(pos+8).toString()+"px"});
+        $("#preview-image-new").css({"left":(pos*-1).toString()+"px"});
+    }});
 
-    try{
-        var buttonYes = activeSlide[0].getElementsByClassName("button-primary");
-        var buttonNo = activeSlide[0].getElementsByClassName("button-secondary");
+    $('#congratulation-container').hide();
+    $('#congratulation-ilustration').hide();
+    $('#congratulation-textblock').hide();
 
-        buttonYes[0].classList.add("button-primary-active");
-        buttonNo[0].classList.add("button-secondary-active");
+    $('#cta-container').hide();
+    $('#cta-ilustration').hide();
+    $('#cta-textblock').hide();
 
-        buttonYes[0].classList.remove("button-primary-disabled");
-        buttonNo[0].classList.remove("button-secondary-disabled");
-    }
-    catch(exeption){
-        transitionQuiz();
-    }
+    quizSwiper.on('slideChange', function () {
+        try{
+            var buttonYes = $(".swiper-slide-next .button-primary");
+            var buttonNo = $(".swiper-slide-next .button-secondary");
+    
+            buttonYes[0].classList.add("button-primary-active");
+            buttonNo[0].classList.add("button-secondary-active");
+    
+            buttonYes[0].classList.remove("button-primary-disabled");
+            buttonNo[0].classList.remove("button-secondary-disabled");
+        }
+        catch(exeption){
+            transitionQuiz();
+        }
+    });
 });
 
 function confirmationYes(button) {
     if(button.classList.contains("button-primary-active")) {
-        swiper.slideNext();
+        quizSwiper.slideNext();
         quizRight++;
     }
 }
 function confirmationNo(button) {
     if(button.classList.contains("button-secondary-active")) {
-        swiper.slideNext();
+        quizSwiper.slideNext();
     }
 }
 
@@ -71,3 +77,21 @@ function transitionQuiz(){
         }
     });
 }
+
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+$(window).on('resize scroll', function() {
+    if ($('#gallery-animation-target').isInViewport()) {
+        setTimeout(function() {
+            $("#gallery-card-client").animate({opacity: '100%', marginLeft: '0px'}, 1000, 'swing');
+        }, 500);
+    }
+});
